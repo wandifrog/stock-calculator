14ac25b2e743143aa5c61a2fb100a47043c48af4
Wandi <wandifrog@gmail.com>
2020-12-03 22:55:26 +0700

    feat: create ui and calculation function

--- src/script.js
+++ src/script.js
@@ -3,21 +3,24 @@ const input = document.getElementById('input')
 const slider = document.getElementById('slider')
 
 const outsideState = {
-  defaultValue: 1475,
-  typingTimeout: null
+  defaultStockValue: 1475,
+  defaultTotalLot: 10,
+  typingTimeout: null,
 }
 
 const App = () => {
 
   const [state, setState] = React.useState({
-    value: outsideState.defaultValue,
+    stockValue: outsideState.defaultStockValue,
+    totalLot: outsideState.defaultTotalLot,
     min: 0,
     max: 0,
     step: 5,
+    stocks: getStocks(outsideState.defaultStockValue, 10, 5)
   })
 
   React.useEffect(() => {
-    handleChangeStockValue(outsideState.defaultValue)
+    handleChangeStockValue(outsideState.defaultStockValue)
   }, [])
 
   const handleChangeStockValue = (stockValue) => {
@@ -28,19 +31,30 @@ const App = () => {
     } else {
       step = 5
     }
+
+    const stocks = getStocks(stockValue, state.totalLot, state.step)
+
     const newState = {
       ...state,
       step,
-      value: stockValue,
+      stockValue,
       min: stockValue - (step * 20),
-      max: stockValue + (step * 20)
+      max: stockValue + (step * 140),
+      stocks
     }
 
     setState(newState)
   }
 
-  const asd2 = (value) => {
-    setState({ ...state, value })
+  const handleChangeTotalLot = (totalLot) => {
+    totalLot = +totalLot
+
+    setState({ ...state, totalLot })
+  }
+
+  const handleSlider = (stockValue) => {
+    const stocks = getStocks(stockValue, state.totalLot, state.step)
+    setState({ ...state, stockValue, stocks })
   }
 
   console.log('state', state)
@@ -49,36 +63,80 @@ const App = () => {
     <div className="container d">
       <div className="row">
         <div className="col-sm">
-          <h5>Stock Price</h5>
+          <h5>Stock Price (buy)</h5>
           <input type="number" className="form-control col-3" id="input"
-            step={state.step} placeholder="Input Stock Here" defaultValue={state.value}
+            step={state.step} placeholder="Input Stock Here" defaultValue={state.stockValue}
             noValidate onChange={(e) => handleChangeStockValue(e.target.value)}
           />
-          <h5>Total Lot</h5>
+          <h5 style={{ marginTop: 10 }}>Total Lot</h5>
           <input type="number" className="form-control col-3" id="input"
-            step={state.step} placeholder="(optional)" defaultValue=""
+            step="1" placeholder="(optional)" defaultValue=""
             noValidate onChange={(e) => handleChangeStockValue(e.target.value)}
           />
         </div>
         <div className="col-sm">
           <div className="row mb-3">
             <label htmlFor="customRange3" className="form-label col-2">Example range</label>
-            <div className="col-9">
-
-            </div>
           </div>
-          <input type="range" className="form-range" value={state.value}
-            min={state.min} max={state.max} step={state.step} id="slider" onChange={(e) => asd2(e.target.value)} />
-          <div>{state.value}</div>
+          <input type="range" className="form-range" value={state.stockValue}
+            min={state.min} max={state.max} step={state.step} id="slider" onChange={(e) => handleSlider(e.target.value)} />
+          <div>{state.stockValue}</div>
         </div>
         <div className="col-sm">
         </div>
       </div>
+      <table className="table mt-10" style={{ marginTop: 50 }}>
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Price</th>
+            <th scope="col">Percentage</th>
+            <th scope="col">Cuan</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            state.stocks.map((item) => {
+
+              return (
+                <tr key={item.no}>
+                  <th scope="row">{item.no}</th>
+                  <td>{item.stock}</td>
+                  <td>{item.percentage}%</td>
+                  <td>{item.gain}</td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
     </div>
   )
 }
 
-console.clear()
+function getStocks(startingStock, totalLot = 0, step = 5) {
+  const stocks = []
+
+  for (
+    let counter = -2, no = 1, stock = startingStock - (2 * step);
+    counter < 13;
+    counter++, no++, stock += step
+  ) {
+    const percentage = ((stock - startingStock) / startingStock * 100).toFixed(2)
+    const gain = (totalLot * stock * percentage).toFixed()
+    stocks.push({
+      no,
+      stock,
+      percentage,
+      gain
+    })
+
+  }
+
+  return stocks
+}
+
+// console.clear()
 
 function separate() {
   console.log('-----------------')
@@ -115,11 +173,9 @@ function percents(a) {
 }
 
 
-getPercent(1475, 1490)
-cuan(15147, 1.26)
-percents(49802000)
-// 0.3 0.3 0.66 1
-
+// getPercent(1475, 1490)
+// cuan(15147, 1.26)
+// percents(49802000)
 
 
 
